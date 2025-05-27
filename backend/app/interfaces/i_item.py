fd5bc8da04b3e214f11f2855999d738e583a289c
Huda Febrianto Nurrohman <febriantohuda@gmail.com>
2025-05-27 14:39:43 +0700

    🚨 Remove linter warning on interfaces

--- backend/app/interfaces/i_item.py
+++ backend/app/interfaces/i_item.py
@@ -1,9 +1,10 @@
 import uuid
+from abc import ABC
 
 from app.models import Item
 
 from .i_base import IBase
 
 
-class IItem(IBase[Item, uuid.UUID]):
+class IItem(IBase[Item, uuid.UUID], ABC):
     pass
--- backend/app/interfaces/i_user.py
+++ backend/app/interfaces/i_user.py
@@ -1,9 +1,10 @@
 import uuid
+from abc import ABC
 
 from app.models import User
 
 from .i_base import IBase
 
 
-class IUser(IBase[User, uuid.UUID]):
+class IUser(IBase[User, uuid.UUID], ABC):
     pass
